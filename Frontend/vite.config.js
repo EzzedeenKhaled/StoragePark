8bfac0c2ce710760ac85c6115da484c27b66eea5
AbedalazizMostapha <abedalazizmos.123@gmail.com>
2025-05-01 10:53:01 +0300

    VITE config only.

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,46 +8,11 @@ export default defineConfig({
 		tailwindcss(),
 		react()
 	],
-	optimizeDeps: {
-		include: ['react-map-gl', 'mapbox-gl'],
-		force: true,
-		esbuildOptions: {
-			define: {
-				global: 'globalThis'
-			}
-		}
-	},
-	resolve: {
-		alias: {
-			'mapbox-gl': 'mapbox-gl/dist/mapbox-gl'
-		}
-	},
 	css: {
-		modules: false,
 		postcss: {
 			plugins: [
 				autoprefixer
 			]
-		}
-	},
-	build: {
-		rollupOptions: {
-			external: ['mapbox-gl']
-		}
-	},
-	define: {
-		'process.env': {}
-	},
-	server: {
-		host: '0.0.0.0',
-		port: 5173,
-		strictPort: true,
-		hmr: {
-			protocol: 'ws',
-			host: 'localhost',
-			port: 5173,
-			clientPort: 5173,
-			overlay: false
 		}
 	}
 });
